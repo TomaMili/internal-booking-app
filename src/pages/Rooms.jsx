959ff7f47c052f0442a55b498093c8156c141429
Toma Milićević <tomatomamilicevic@gmail.com>
2025-06-12 04:17:50 +0200

    fix: add trailing space in Rooms component for consistency

--- src/pages/Rooms.jsx
+++ src/pages/Rooms.jsx
@@ -2,7 +2,7 @@ import RoomsGrid from "../features/rooms/RoomsGrid";
 
 function Rooms() {
   return (
-    <div className="flex-1 min-h-0">
+    <div className="flex-1 min-h-0 ">
       <RoomsGrid />
     </div>
   );
