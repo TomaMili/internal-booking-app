34775cf6ad121d1cb04fc12d334f565d24c7530a
Toma Milićević <tomatomamilicevic@gmail.com>
2025-06-15 22:55:09 +0200

    fix: correct image source path in Sidebar component

--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -8,7 +8,7 @@ function Sidebar({ onHover, isNavActive }) {
       className="bg-zinc-0 h-screen z-2 shadow-[0_0_15px_-12px] shadow-zinc-900 bg-emerald-0 flex flex-col items-center gap-6"
     >
       <img
-        src="public/vite.svg"
+        src="/vite.svg"
         width="60%"
         className={`mx-4 h-1/8 mt-3 transition-all duration-300 `}
       />
