cabfc0d3d699d7988da366d6d244e8c378bff64f
Toma Milićević <tomatomamilicevic@gmail.com>
2025-06-20 23:08:35 +0200

    fix: update Sidebar component to use min-height for better responsiveness

--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -5,7 +5,7 @@ function Sidebar({ onHover, isNavActive }) {
     <aside
       onMouseEnter={() => onHover(true)}
       onMouseLeave={() => onHover(false)}
-      className="bg-zinc-0 h-screen z-2 shadow-[0_0_15px_-12px] shadow-zinc-900 bg-emerald-0 flex flex-col items-center gap-6"
+      className="bg-zinc-0 min-h-dvh z-2 shadow-[0_0_15px_-12px] shadow-zinc-900 bg-emerald-0 flex flex-col items-center gap-6"
     >
       <img
         src="/vite.svg"
