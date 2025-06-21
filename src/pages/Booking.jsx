cff3a4b3dfd459b25189344f1a71a897813e96a7
Toma Milićević <tomatomamilicevic@gmail.com>
2025-06-21 02:47:03 +0200

    feat: add Booking and CheckIn components for booking management

---
+++ src/pages/Booking.jsx
@@ -0,0 +1,11 @@
+import BookingPage from "../features/bookings/BookingPage";
+
+function Booking() {
+  return (
+    <div>
+      <BookingPage />
+    </div>
+  );
+}
+
+export default Booking;
---
+++ src/pages/CheckIn.jsx
@@ -0,0 +1,11 @@
+import CheckinPage from "../features/check-in-out/CheckInPage";
+
+function CheckIn() {
+  return (
+    <div>
+      <CheckinPage />
+    </div>
+  );
+}
+
+export default CheckIn;
