820f019b29887d0c08ab4e9237d346aba15d1ea3
Toma Milićević <tomatomamilicevic@gmail.com>
2025-06-21 04:13:58 +0200

    feat: add CheckIn and Booking routes to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Toaster } from "react-hot-toast";
+
 import Dashboard from "./pages/Dashboard";
 import Bookings from "./pages/Bookings";
 import Rooms from "./pages/Rooms";
@@ -6,12 +10,11 @@ import Users from "./pages/Users";
 import Settings from "./pages/Settings";
 import Account from "./pages/Account";
 import Login from "./pages/Login";
+import CheckIn from "./pages/CheckIn";
+import Booking from "./pages/Booking";
 import PageNotFound from "./pages/PageNotFound";
+
 import AppLayout from "./ui/AppLayout";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { CircleSpinner } from "react-spinners-kit";
-import { Toaster } from "react-hot-toast";
 
 // React query setup pt1
 const queryClient = new QueryClient({
@@ -32,6 +35,8 @@ function App() {
             <Route index element={<Navigate replace to="dashboard" />} />
             <Route path="dashboard" element={<Dashboard />} />
             <Route path="bookings" element={<Bookings />} />
+            <Route path="bookings/:bookingId" element={<Booking />} />
+            <Route path="checkin/:bookingId" element={<CheckIn />} />
             <Route path="rooms" element={<Rooms />} />
             <Route path="users" element={<Users />} />
             <Route path="settings" element={<Settings />} />
