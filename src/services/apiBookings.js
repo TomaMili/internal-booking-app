8711e61b3a95c92d5ac3dd014103f3118639410e
Toma Milićević <tomatomamilicevic@gmail.com>
2025-06-21 02:46:27 +0200

    refactor: consolidate booking-related functions for improved readability and maintainability

--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -16,3 +16,40 @@ export async function getBookings({ filter, sort }) {
 
   return data;
 }
+
+export async function getBooking(id) {
+  const { data, error } = await supabase
+    .from("bookings")
+    .select("*, rooms(*), guests(*)")
+    .eq("id", id)
+    .single();
+
+  if (error) {
+    throw new Error("Bookings error:", error.message);
+  }
+
+  return data;
+}
+
+export async function updateBooking(id, obj) {
+  const { data, error } = await supabase
+    .from("bookings")
+    .update(obj)
+    .eq("id", id)
+    .select()
+    .single();
+
+  if (error) {
+    throw new Error("Bookings error:", error.message);
+  }
+  return data;
+}
+
+export async function deleteBooking(id) {
+  const { data, error } = await supabase.from("bookings").delete().eq("id", id);
+
+  if (error) {
+    throw new Error("Bookings error:", error.message);
+  }
+  return data;
+}
