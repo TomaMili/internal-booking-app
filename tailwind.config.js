0fc89bfe05e8a8f8fb793448fc90fce87966cc6f
Toma Milićević <tomatomamilicevic@gmail.com>
2025-09-19 18:24:45 +0200

    style: enhance CSS variables for light and dark modes in index.css; update tailwind.config.js formatting

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
-// /* eslint-env node */
-// /** @type {import('tailwindcss').Config} */
-// // eslint-disable-next-line no-undef
-// module.exports = {
-//   content: ["./src/**/*.{js,jsx,ts,tsx}"],
-//   safelist: [
-//     {
-//       pattern: /grid-cols-\[.*\]/, // allow dynamic grid-cols-[...] classes
-//     },
-//   ],
-// };
+/* eslint-env node */
+/** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line no-undef
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  safelist: [
+    {
+      pattern: /grid-cols-\[.*\]/, // allow dynamic grid-cols-[...] classes
+    },
+  ],
+};
