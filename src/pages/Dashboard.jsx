e28d7a2c5d5107ba746188c85f71f25328c449c5
Toma Milićević <tomatomamilicevic@gmail.com>
2025-09-19 13:26:58 +0200

    feat: update Dashboard component layout and integrate DashboardFilter and DashboardLayout

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,10 +1,16 @@
-import Button from "../ui/Button";
+import DashboardFilter from "../features/dashboard/DashboardFilter";
+import DashboardLayout from "../features/dashboard/DashboardLayout";
 
 function Dashboard() {
   return (
-    <div>
-      <Button type="primary">Button</Button>
-    </div>
+    <>
+      <div className="flex items-center justify-between mb-6">
+        <h1 className="font-medium text-5xl mb-3 ml-2">Dashboard</h1>
+        <DashboardFilter />
+      </div>
+
+      <DashboardLayout />
+    </>
   );
 }
 
