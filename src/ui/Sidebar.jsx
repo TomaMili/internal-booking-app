30614bc22887c60c3bf7934d6d6808fcda850538
Toma Milićević <tomatomamilicevic@gmail.com>
2025-09-21 17:58:20 +0200

    feat: update Sidebar component to use SVG logos based on dark mode preference
    - Integrated DarkModeContext to determine the current theme.
    - Replaced static logo with MiniLogoDark and MiniLogoLight SVGs for better theme adaptability.
    - Updated logo styling for responsive behavior based on navigation state.
    
    chore: add SVGR plugin to Vite configuration
    
    - Included vite-plugin-svgr to enable importing SVGs as React components.

--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -1,20 +1,31 @@
+import { useDarkMode } from "../context/DarkModeContext";
 import Navigation from "./Navigation";
+import MiniLogoDark from "../assets/mini-logo-dark-cropped.svg?react";
+import MiniLogoLight from "../assets/mini-logo-light-cropped.svg?react";
 
 function Sidebar({ onHover, isNavActive }) {
+  const { isDarkMode } = useDarkMode();
+
   return (
     <aside
       onMouseEnter={() => onHover(true)}
       onMouseLeave={() => onHover(false)}
       className="bg-zinc-0 min-h-dvh z-2 shadow-[0_0_15px_-12px] shadow-zinc-900 bg-white flex flex-col items-center gap-6"
     >
-      <img
-        src="/logo.png"
-        width={isNavActive ? "70%" : "100%"}
-        height="40px"
-        className={`mx-4 ${
-          isNavActive ? "mt-10" : "mt-10.5"
-        } transition-all duration-300 object-contain`}
-      />
+      {isDarkMode ? (
+        <MiniLogoDark
+          className={`px-4 ${
+            isNavActive ? "mt-10 w-[70%]" : "mt-10 w-full !h-20"
+          } h-40 transition-all duration-300 object-contain`}
+        />
+      ) : (
+        <MiniLogoLight
+          className={`px-4 ${
+            isNavActive ? "mt-10 w-[70%]" : "mt-10 w-full !h-20"
+          } h-40 transition-all duration-300 object-contain`}
+        />
+      )}
+
       <Navigation isNavActive={isNavActive} />
     </aside>
   );
